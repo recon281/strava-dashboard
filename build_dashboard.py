4923ff0424d57eaad63b19bdb5d59458c2acd209
recon281 <gabulko@gmail.com>
2026-08-19 19:15:55 +0200

    Update build_dashboard.py

--- build_dashboard.py
+++ build_dashboard.py
@@ -714,8 +714,8 @@ MANIFEST = {
     "background_color": "#ffffff",
     "theme_color": "#fc4c02",
     "icons": [
-        {"src": "icon-192.png", "sizes": "192x192", "type": "image/png"},
-        {"src": "icon-512.png", "sizes": "512x512", "type": "image/png"},
+        {"src": "pulse_app_icon_180.png", "sizes": "192x192", "type": "image/png"},
+        {"src": "pulse_app_icon_1024.png", "sizes": "512x512", "type": "image/png"},
     ],
 }
 
